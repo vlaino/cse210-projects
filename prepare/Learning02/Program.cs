a0d05016c62833ef560489c84ab7b0692ce4da10
vlaino <vanee.laino@gmail.com>
2024-09-30 21:17:44 -0300

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "MEF";
+        job1._jobTitle = "Accountant";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+      
+
+        Job job2 = new Job();
+        job2._company = "Cámara de Senadores";
+        job2._jobTitle = "Financial Advice";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+        
+        Resume myResume = new Resume();
+        myResume._name = "Vanesa Laino";
+        
+        myResume._jobs.Add(job1); 
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
