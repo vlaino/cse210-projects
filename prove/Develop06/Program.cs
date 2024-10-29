9e89084f823e72b005bc5aa22eb9aac5c3f2ecb7
vlaino <vanee.laino@gmail.com>
2024-10-29 18:21:21 -0300

    Eternal Quest

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        Console.WriteLine("Welcome to the Eternal Quest Program!");
         GoalManager goalManager = new();
         goalManager.Start();
     }
